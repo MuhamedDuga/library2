a76fb105c1beb51c21a9256b495e4b075c34604c
qachicago <tiresfx@gmail.com>
2023-03-05 21:35:06 -0600

    US01_UserTable_StepDefs created @wip

---
+++ src/test/java/com/library2/pages/LibrarianMainPage.java
@@ -0,0 +1,18 @@
+package com.library2.pages;
+
+import com.library2.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LibrarianMainPage {
+
+    public LibrarianMainPage(){
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    @FindBy(xpath="//a[@href='#users']")
+    public WebElement UsersLink;
+
+
+}
