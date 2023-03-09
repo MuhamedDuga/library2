e7ad2cca11255a6f80c817c10a5546f0880fc8a8
MD <dugonjicm@icloud.com>
2023-03-08 21:52:21 -0500

    US03_MD

--- src/test/java/com/library2/pages/ManageUserInfoTablePage_MD.java
+++ src/test/java/com/library2/pages/ManageUserInfoTablePage_MD.java
@@ -1,21 +1,41 @@
 package com.library2.pages;
 
 import com.library2.utilities.Driver;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
 
 public class ManageUserInfoTablePage_MD {
     public ManageUserInfoTablePage_MD (){
         PageFactory.initElements(Driver.getDriver(), this);
+
     }
+    Select select;
+
     @FindBy(xpath="//span[.='Users']")
     public WebElement users;
 
     @FindBy(name = "tbl_users_length")
-    public WebElement showRecordsDropdown;
+    public WebElement showNumDropdown;
 
-    @FindBy(xpath="//div[@class='dataTables_length']//option[4]")
-    public WebElement showRecords;
+   public void findRowNumDropdown() {
+       select = new Select(showNumDropdown);
+   }
+   public void selectFromDropdown(Integer integer) {
+       findRowNumDropdown();
+       select.selectByValue(integer + "");
+   }
+   public String valueOfDropdown() {
+       findRowNumDropdown();
+       return select.getFirstSelectedOption().getText();
+   }
+   public String numOfRows() {
+       findRowNumDropdown();
+      WebElement num = Driver.getDriver().findElement(By.xpath("//div[@class='dataTables_length']//option[4]"));
+        return num.isDisplayed() ;
+   }
+
 
 }
--- src/test/java/com/library2/step_definitions/US03_ManageUserInfoTable_StepDefs.java
+++ src/test/java/com/library2/step_definitions/US03_ManageUserInfoTable_StepDefs.java
@@ -13,10 +13,10 @@ import org.openqa.selenium.support.ui.Select;
 public class US03_ManageUserInfoTable_StepDefs {
     LoginPage loginPage = new LoginPage();
     ManageUserInfoTablePage_MD userInfoTable = new ManageUserInfoTablePage_MD();
-    Select select = new Select(userInfoTable.showRecordsDropdown);
+
     @Given("I am on the login page")
     public void i_am_on_the_login_page() {
-        Driver.getDriver().get(ConfigurationReader.getProperty("url"));
+
     }
     @Given("I login as a librarian")
     public void i_login_as_a_librarian() {
@@ -29,16 +29,16 @@ public class US03_ManageUserInfoTable_StepDefs {
         userInfoTable.users.click();
     }
     @When("I select Show {int} records")
-    public void i_select_show_records(Integer int1) {
-        select.selectByValue((int1 + ""));
+    public void i_select_show_records(Integer integer) {
+        userInfoTable.selectFromDropdown(integer);
     }
     @Then("show records value should be {int}")
-    public void show_records_value_should_be(Integer int1) {
-        Assert.assertEquals(int1 + "", select.getFirstSelectedOption().getText());
+    public void show_records_value_should_be(Integer integer) {
+        Assert.assertEquals(userInfoTable.valueOfDropdown(), integer);
     }
     @Then("the users table must display {int} records")
-    public void the_users_table_must_display_records(Integer int1) {
-        Assert.assertEquals((int)int1, userInfoTable.showRecords.isDisplayed());
+    public void the_users_table_must_display_records(Integer integer) {
+        Assert.assertEquals((userInfoTable.numOfRows(), integer+ "");
     }
 
 }
