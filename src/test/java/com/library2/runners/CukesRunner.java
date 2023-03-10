5d160157b95543ed86e6e57ff54df426820c7c0c
MD <dugonjicm@icloud.com>
2023-03-10 02:27:09 -0500

    US03_MD

--- src/test/java/com/library2/runners/CukesRunner.java
+++ src/test/java/com/library2/runners/CukesRunner.java
@@ -15,7 +15,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/library2/step_definitions",
         dryRun = false,
-        tags = "@LateNight",
+        tags = "",
         publish = true //generating a report with public link
 )
 public class CukesRunner {}
